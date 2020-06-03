90c41d7de4ff0757daaab92bc144c034258c832b
Fernanda Caroline <fernandacaroline2008@gmail.com>
2020-06-03 17:34:33 -0600

    Fix conflict between cors and swagger

--- src/main/java/com/fernanda/comment_service/config/WebConfig.java
+++ src/main/java/com/fernanda/comment_service/config/WebConfig.java
@@ -2,23 +2,13 @@ package com.fernanda.comment_service.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
+        registry.addMapping("/**");
     }
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-    }
 }
